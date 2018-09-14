536a056de830b66cdf60aaef7a658ab89bb308bd
Gonzalo Orozco <gonzalo.gorozco@mercadolibre.com>
2018-09-14 13:43:25 -0300

    adding heroku support

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -32,14 +33,6 @@ hbs.registerHelper('screamIt', (text) => {
 });
 
 app.get('/', (req, res) => {
-	//res.send('<h1>Hello Express!</h1>');
-	// res.send({
-	// 	name: 'Gonzalo',
-	// 	likes: [
-	// 		'Libros',
-	// 		'futbol'
-	// 	]	
-	// });
 
 	res.render('home.hbs', {
 		pageTitle: 'Home Page',
@@ -60,6 +53,6 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log('Server is up on port 3000');
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
 });
